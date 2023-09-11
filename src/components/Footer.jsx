557311800a704a84783373647f4601ab2013c259
Kyryl Sherstobaiev <novemberua@gmail.com>
2023-09-11 11:48:15 +0300

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 function Footer() {
-    return(
+    return (
         <footer className="page-footer green darken-1">
             <div className="footer-copyright">
                 <div className="container">
                     © {new Date().getFullYear()} Copyright Text
-                    <a className="grey-text text-lighten-4 right" href="#">Repo</a>
+                    <a className="grey-text text-lighten-4 right"
+                       href="https://kyrylsherstobaiev.github.io/react-shop-fortnite-small/" rel="noreferrer"
+                       target="_blank">Repo</a>
                 </div>
             </div>
         </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ function Header() {
     return (
         <nav className="green darken-3">
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo">React Shop</a>
+                <a href="/" className="brand-logo">React Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="#">Repo</a></li>
+                    <li><a href="https://kyrylsherstobaiev.github.io/react-shop-fortnite-small/" rel="noreferrer"
+                           target='_blank'>Repo</a></li>
                 </ul>
             </div>
         </nav>
